23c9573ce43c60342cae4cb3807549c47301b2ff
Agogic-Alephon <malachizachary@gmail.com>
2023-03-20 19:24:25 -0500

    added db_structures sql file that outlines the structure of the db for use on SBC server

---
+++ db_structure.sql
@@ -0,0 +1,46 @@
+CREATE TABLE users 
+		(
+			id SERIAL PRIKMARY KEY,
+			name VARCHAR ( 255 ) NOT NULL,
+			email VARCHAR ( 255 ) UNIQUE NOT NULL,
+			password VARCHAR ( 255 ) NOT NULL,
+		);
+CREATE TABLE workspaces 
+		(
+			workspace_id SERIAL PRIMARY KEY,
+			owner_id INT NOT NULL,
+			FOREIGN KEY (owner_id)
+				REFERENCES users (id)
+		);
+CREATE TABLE boards 
+		(
+			board_id SERIAL PRIMARY KEY,
+			board_name VARCHAR ( 255 ) NOT NULL,
+			workspace_id INT NOT NULL,
+			FOREIGN KEY (workspace_id) 
+				REFERENCES workspces (workspace_id)
+		);
+CREATE TABLE columns
+		(
+			column_id SERIAL PRIMARY KEY,	
+			column_name VARCHAR ( 255 ) NOT NULL,
+			column_placement INT NOT NULL,
+			board_id INT NOT NULL,
+			FOREIGN KEY (board_id) 
+				REFERENCES boards (board_id)
+		);
+CREATE TABLE tasks 
+		(
+			task_id SERIAL PRIMARY KEY,
+			task_name VARCHAR ( 255 ),
+			description VARCHAR ( 255 ),
+			task_placement INT NOT NULL,
+			color VARCHAR ( 255 ),
+			column_number INT NOT NULL,
+			coumn_id INT NOT NULL,
+			creator_id INT,
+			FOREIGN KEY (column_id)
+				REFERENCES columns (column_id),
+			FOREIGN KEY (creator_id) 
+				REFERENCES users (id),
+		);
