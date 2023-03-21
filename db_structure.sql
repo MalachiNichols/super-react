dc13520f72b746179f732b59239a90930d3b6c99
Agogic-Alephon <malachizachary@gmail.com>
2023-03-20 19:33:59 -0500

    more sql formatting

--- db_structure.sql
+++ db_structure.sql
@@ -1,20 +1,20 @@
 CREATE TABLE users 
 		(
-			id SERIAL PRIMARY KEY,
+			id serial PRIMARY KEY,
 			name VARCHAR ( 255 ) NOT NULL,
 			email VARCHAR ( 255 ) UNIQUE NOT NULL,
-			password VARCHAR ( 255 ) NOT NULL,
+			password VARCHAR ( 255 ) NOT NULL
 		);
 CREATE TABLE workspaces 
 		(
-			workspace_id SERIAL PRIMARY KEY,
+			workspace_id serial PRIMARY KEY,
 			owner_id INT NOT NULL,
 			FOREIGN KEY (owner_id)
 				REFERENCES users (id)
 		);
 CREATE TABLE boards 
 		(
-			board_id SERIAL PRIMARY KEY,
+			board_id serial PRIMARY KEY,
 			board_name VARCHAR ( 255 ) NOT NULL,
 			workspace_id INT NOT NULL,
 			FOREIGN KEY (workspace_id) 
@@ -22,7 +22,7 @@ CREATE TABLE boards
 		);
 CREATE TABLE columns
 		(
-			column_id SERIAL PRIMARY KEY,	
+			column_id serial PRIMARY KEY,	
 			column_name VARCHAR ( 255 ) NOT NULL,
 			column_placement INT NOT NULL,
 			board_id INT NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE columns
 		);
 CREATE TABLE tasks 
 		(
-			task_id SERIAL PRIMARY KEY,
+			task_id serial PRIMARY KEY,
 			task_name VARCHAR ( 255 ),
 			description VARCHAR ( 255 ),
 			task_placement INT NOT NULL,
@@ -42,5 +42,5 @@ CREATE TABLE tasks
 			FOREIGN KEY (column_id)
 				REFERENCES columns (column_id),
 			FOREIGN KEY (creator_id) 
-				REFERENCES users (id),
+				REFERENCES users (id)
 		);
