43ff8508206ca73ed98a4470c2f43f5a2da680fb
Agogic-Alephon <malachizachary@gmail.com>
2023-03-21 20:25:55 -0500

    adjusted all fetch reqs to reflect new https model

--- client/src/pages/auth/SignIn.js
+++ client/src/pages/auth/SignIn.js
@@ -19,7 +19,7 @@ const SignIn = () => {
   const submitCredentials = async () => {
     console.log(credentials);
     await fetch(
-      `http://${process.env.REACT_APP_LOCALIP}:8080/api/auth/signin`,
+      `https://${process.env.REACT_APP_LOCALIP}:8081/api/auth/signin`,
       {
         method: "POST",
         body: JSON.stringify({
--- client/src/pages/board/Workspace.js
+++ client/src/pages/board/Workspace.js
@@ -26,7 +26,7 @@ const Workspace = ({ checkCredentials, boards, setBoards, setCurrBoard }) => {
     }
 
     setBoards([...boards, title]);
-    await fetch(`http://${process.env.REACT_APP_LOCALIP}:8080/api/boards/save`, {
+    await fetch(`https://${process.env.REACT_APP_LOCALIP}:8081/api/boards/save`, {
       method: "POST",
       body: JSON.stringify({
         boardName: title,
@@ -64,7 +64,7 @@ const Workspace = ({ checkCredentials, boards, setBoards, setCurrBoard }) => {
 
   const deleteBoard = async (title) => {
     console.log(title);
-    await fetch(`http://${process.env.REACT_APP_LOCALIP}:8080/api/boards/delete`, {
+    await fetch(`https://${process.env.REACT_APP_LOCALIP}:8081/api/boards/delete`, {
       method: "DELETE",
       body: JSON.stringify({
         boardName: title,
